e25e987dc6ec7b090e1e210a300a458fa2137e80
Lynn <25660334@qq.com>
2025-01-20 00:04:02 +0800

    perf:change 'static' to 'const',save memory usage

--- src/const_config/mod.rs
+++ src/const_config/mod.rs
@@ -1,18 +1,18 @@
 /// The default server address.
-pub(crate) static DEFAULT_IPV4: &str = "0.0.0.0:9177";
+pub(crate) const DEFAULT_IPV4: &str = "0.0.0.0:9177";
 /// The maximum number of connections allowed by the server.
-pub(crate) static DEFAULT_MAX_CONNECTIONS: usize = 1000;
+pub(crate) const DEFAULT_MAX_CONNECTIONS: usize = 1000;
 /// The maximum number of threads allowed by the server.
-pub(crate) static DEFAULT_MAX_THREADPOOL_SIZE: usize = 10;
+pub(crate) const DEFAULT_MAX_THREADPOOL_SIZE: usize = 10;
 /// The default maximum receive data size for a single client in bytes.
-pub(crate) static DEFAULT_MAX_RECEIVE_BYTES_SIZE: usize = 1024;
+pub(crate) const DEFAULT_MAX_RECEIVE_BYTES_SIZE: usize = 1024;
 /// The default maximum communication channel size for a single client.
 /// To avoid memory overflow in case of network delay, it is set to 3.
-pub(crate) static DEFAULT_CHANNEL_SIZE: usize = 3;
+pub(crate) const DEFAULT_CHANNEL_SIZE: usize = 3;
 /// The default maximum processing permit size for a single client.
 /// Assuming the single processing delay is controlled within 100ms, the server allows processing at least 7 requests from a single client within 100ms.
-pub(crate) static DEFAULT_PROCESS_PERMIT_SIZE: usize = 7;
+pub(crate) const DEFAULT_PROCESS_PERMIT_SIZE: usize = 7;
 /// The interval for heartbeat detection in seconds.
-pub(crate) static DEFAULT_CHECK_HEART_INTERVAL: u64 = 5;
+pub(crate) const DEFAULT_CHECK_HEART_INTERVAL: u64 = 5;
 /// The timeout time for heartbeat detection in seconds.
-pub(crate) static DEFAULT_CHECK_HEART_TIMEOUT_TIME: u64 = 30;
+pub(crate) const DEFAULT_CHECK_HEART_TIMEOUT_TIME: u64 = 30;
